7e388e7ad44d168dbd6a6297095318246902cacc
Hilary Lewis <hilarylewis92@gmail.com>
2016-11-01 16:34:18 -0600

    Login logout component if refactored

--- lib/components/LoginLogout.jsx
+++ lib/components/LoginLogout.jsx
@@ -1,32 +1,41 @@
 import firebase, { reference, signIn, signOut } from '../firebase';
 import React, { Component } from 'react'
 
-export const LoginLogout = ({ user }) => {
-  return(
-    <section className="log-in-out-bar">
-    {
-      user ?
-      <div className="user-welcome-bar">
-        <p className="welcome-contact">Logged in as
-          <span
-            className="user-name-welcome">
-            {user.displayName.split(' ')[0]}
-          </span> ({user.email})
-        </p>
-        <button
-          className="sign-out-button"
-          onClick={() =>
-            signOut()}>
-            Sign Out
-        </button>
-      </div> :
-      <button
-        className="sign-in-button"
-        onClick={() =>
-          signIn()}>
-          Sign In
-      </button>
-    }
-    </section>
-  )
+const LoginLogout = ({user}) => {
+  return (
+    <div className="LoginStatus">
+      {user ?
+        <p>Logged in as
+        <span className ='user-display'>{user.displayName}</span>
+        ({user.email})
+        <button className='LogOut-btn'
+          onClick={()=>signOut()}>Sign Out</button></p>
+        :<button className='LogIn-btn'
+          onClick={()=>signIn()}>Sign In</button>
+      }
+    </div>
+  );
+
 }
+
+module.exports = LoginLogout
+
+// return(
+//   <section className="log-in-out-bar">
+//   {user ?
+//     <div className="user-welcome-bar">
+//       <p className="welcome-contact">Logged in as
+//         <span className="user-name-welcome">{user.displayName}</span> ({user.email})
+//       </p>
+//       <button className="sign-out-button"
+//         onClick={() => signOut()}>
+//           Sign Out
+//       </button>
+//     </div> :
+//     <button className="sign-in-button"
+//       onClick={() => signIn()}>
+//         Sign In
+//     </button>
+//   }
+//   </section>
+// )
